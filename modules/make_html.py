9d35ff208323d3ea2550865a3eec915abddb4037
Peter Bocz <bocz.petya@gmail.com>
2018-10-03 18:09:29 +0100

    functional html builders / under reconstruction

--- modules/make_html.py
+++ modules/make_html.py
@@ -57,88 +57,94 @@ class htmlTable:
         
 class htmlPage:
 
-    def __init__(self, head: str, title_text: list, table_list: list, bg_colour: str):
+    widths = []
+    tabledata = [[]]
+    
+    def __init__(self, head: str, title_text: list, bg_colour='powderblue'):
         self.head = head
         self.title_text = title_text
-        self.table_list = table_list
         self.bg_colour = bg_colour
+        self.filename = '_'
 
     def make_href(self, chfrom: str):
         chto = chfrom.replace('\'', '').replace(' ', _) + '.html'
         return '<a href="{}">{}</a>'.format(chto, chfrom)
+
+    def build_tables(self):
+        t = htmlTable(self.tabledata, self.widths)
+        t_col = t.tcolour('black', '#d3d3d3')
+        t_name = t.tname('_')
+        table = t.assemble(t_name, t_col)
+        return table
+        
 
     def assemble(self):
         opening_tags = '<html>\n<body style="background-color:{};">\n<head><title>\n'.format(self.bg_colour)
         title1 = '<h1 style="text-align: center;">{}</h1>\n'.format(self.title_text[0])
         title2 = '<h2 style="text-align: center;">{}</h2>\n'.format(self.title_text[1])
         body_tags = '</title></head>\n<body>\n'
-        tables = ''.join(self.table_list)
+        tables = ''.join(self.build_tables())
         closing_tags = '</body>\n</html>'
         return opening_tags + self.head + body_tags + title1 + title2  + tables + closing_tags
-    
-class StandingsPage:
-
-    def __init__(self, standings: list, season_name: str):
-        self.standings = standings
-        self.season_name = season_name
 
     def build_page(self):
-        
-        widths = [18,180,100,18,18,18,18,50,50,18,18,18]
-        tabledata = [['P',
-                      'Driver',
-                      'Points',
-                      'Wins',
-                      'Top5s',
-                      'Top10s',
-                      'Poles',
-                      'Avg Finish',
-                      'Avg Start',
-                      'Laps Led',
-                      'DNFs',
-                      'Starts']]
+        with open(self.filename+'.html', 'w') as page_f:
+            page_f.write(self.assemble())
+    
+class StandingsPage(htmlPage):
+
+    widths = [18,250,100,18,18,18,18,50,50,18,18,18]
+    tabledata = [['P',
+                  'Driver',
+                  'Points',
+                  'Wins',
+                  'Top5s',
+                  'Top10s',
+                  'Poles',
+                  'Avg Finish',
+                  'Avg Start',
+                  'Laps Led',
+                  'DNFs',
+                  'Starts']]
+                  
+    def __init__(self, head: str, title_text: list, standings: list, season_name: str):
+        super().__init__(head, title_text)
+        self.standings = standings
+        self.season_name = season_name
+        self.filename = 'standings'
 
         for i, row in enumerate(self.standings):
-            tabledata.append([i+1, *row])
+            self.tabledata.append([i+1, *row])
+                    
 
-        t = htmlTable(tabledata, widths)
-        t_col = t.tcolour('black', '#d3d3d3')
-        t_name = t.tname('Standings')
-        table = t.assemble(t_name, t_col)
-        page = htmlPage('Championship Standings',
-                        [self.season_name, ''],
-                        [table],
-                        'powderblue')
-
-        with open('standings.html', 'w') as page_f:
-            page_f.write(page.assemble())
-        #webbrowser.open('standings.html')
-
-class RaceResults:
+class RaceResults(htmlPage):
     ''' The html format of a race results of a given season. '''
-    
-    def __init__(self, results: dict, season_name: str, event_name):
+
+    widths = [18,18,65,18,180,65,45,45,60,50,50]
+    tabledata = [['P',
+                   'S',
+                   'Q Time',
+                   '#',
+                   'Driver',
+                   'Interval',
+                   'Laps',
+                   'Led',
+                   'Led Most',
+                   'Points',
+                   'Status']]
+                   
+    def __init__(self, head, title_text, results: dict, season_name: str, event_name):
+        super().__init__(head, title_text)
         self.results = results
         self.season_name = season_name
         self.event_name = event_name
+        self.filename = 'race' + event_name.replace(' ', '_') \
+                                           .replace('\'', '') \
+                                           .replace('(', '_') \
+                                           .replace(')', '_') 
 
-    def build_page(self):
-        ''' Builds the webpage, saves the file and opens it. '''
-
-        widths = [18,18,65,18,180,65,45,45,60,50,50]
-        tabledata = [['P',
-                       'S',
-                       'Q Time',
-                       '#',
-                       'Driver',
-                       'Interval',
-                       'Laps',
-                       'Led',
-                       'Led Most',
-                       'Points',
-                       'Status']]
         for row in self.results:
-            tabledata.append([row['r_position'],
+            self.tabledata.append([row['r_position'],
                               row['q_position'],
                               row['q_time'],
                               row['#'],
@@ -151,37 +157,24 @@ class RaceResults:
                               row['status']
                               ])
 
-        t = htmlTable(tabledata, widths)
-        t_col = t.tcolour('black', '#d3d3d3')
-        t_name = t.tname('Results')
-        table = t.assemble(t_name, t_col)
-        page = htmlPage('Race Results',
-                        [self.season_name, self.event_name],
-                        [table],
-                        'powderblue')
-
-        with open('race.html', 'w') as page_f:
-            page_f.write(page.assemble())
-        #webbrowser.open('race.html')
         
-class DriverStats:
+class DriverStats(htmlPage):
+    
+    widths = [18, 300, 300, 18, 18, 18, 45, 80]
+    tabledata = [['No.',
+                  'Event',
+                  'Track',
+                  'Finish',
+                  'Start',
+                  'Laps Led',
+                  'Most Led',
+                  'Status']]
 
     def __init__(self, driver_name: str, driver_stats: list):
         self.driver_name = driver_name
         self.driver_stats = driver_stats
 
     def build_page(self):
-        
-        widths = [18, 300, 300, 18, 18, 18, 45, 80]
-        tabledata = [['No.',
-                      'Event',
-                      'Track',
-                      'Finish',
-                      'Start',
-                      'Laps Led',
-                      'Most Led',
-                      'Status']]
-
         
         for event in self.driver_stats:
             tabledata.append(event)
@@ -199,8 +192,14 @@ class DriverStats:
         with open(filename+'.html', 'w') as page_f:
             page_f.write(page.assemble())
     
-class SeasonInfo:
+class SeasonInfo(htmlPage):
     ''' The html format of the current season's data. '''
+    widths = [160, 160, 100, 45, 160]
+    tabledata = [['Date',
+                  'Event Name',
+                  'Track',
+                  'Laps',
+                  'Winner']]
     
     def __init__(self, season_info: dict, winners: dict):
         self.season_info = season_info
@@ -208,13 +207,6 @@ class SeasonInfo:
 
     def build_page(self):
         ''' Builds the webpage, saves the file and opens it. '''
-        
-        widths = [160, 160, 100, 45, 160]
-        tabledata = [['Date',
-                      'Event Name',
-                      'Track',
-                      'Laps',
-                      'Winner']]
 
         for row in self.season_info['event_list']:
             print(row['event_name'])
@@ -225,7 +217,7 @@ class SeasonInfo:
                               self.winners[row['event_name']]['winner']
                               ])
 
-        t = htmlTable(tabledata, widths)
+        t = htmlTable(self.tabledata, self.widths)
         t_col = t.tcolour('black', '#d3d3d3')
         t_name = t.tname('Season')
         table = t.assemble(t_name, t_col)
@@ -236,4 +228,3 @@ class SeasonInfo:
 
         with open('season.html', 'w') as page_f:
             page_f.write(page.assemble())
-        #webbrowser.open('season.html')
